bac91e78615a4e17a7854b7f9c6ec7fba5fcb232
PiotrDuma <piotrduma2014@gmail.com>
2024-01-25 00:03:34 +0100

    add PaymentPeriod value object

--- src/main/java/com/github/PiotrDuma/payroll/domain/payment/classification/PaymentClassification.java
+++ src/main/java/com/github/PiotrDuma/payroll/domain/payment/classification/PaymentClassification.java
@@ -1,5 +1,8 @@
 package com.github.PiotrDuma.payroll.domain.payment.classification;
 
+import com.github.PiotrDuma.payroll.common.Salary;
+import com.github.PiotrDuma.payroll.domain.payment.schedule.PaymentPeriod;
+
 public interface PaymentClassification {
-  //TODO:
+  Salary calculatePay(PaymentPeriod paymentPeriod);
 }
---
+++ src/main/java/com/github/PiotrDuma/payroll/domain/payment/schedule/PaymentPeriod.java
@@ -0,0 +1,14 @@
+package com.github.PiotrDuma.payroll.domain.payment.schedule;
+
+import java.time.LocalDate;
+
+public record PaymentPeriod(LocalDate startPeriod, LocalDate endPeriod) {
+
+  private static final String MESSAGE = "Invalid time period";
+
+  public PaymentPeriod {
+    if (startPeriod.isAfter(endPeriod) || endPeriod.isBefore(startPeriod)) {
+      throw new IllegalArgumentException(MESSAGE);
+    }
+  }
+}
