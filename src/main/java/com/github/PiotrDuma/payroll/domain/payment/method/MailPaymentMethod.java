e489e9161674aa1a8ce5488f7a4bb93d464992a8
PiotrDuma <piotrduma2014@gmail.com>
2024-01-31 15:18:15 +0100

    add MailPaymentMethod

---
+++ src/main/java/com/github/PiotrDuma/payroll/domain/payment/method/MailPaymentMethod.java
@@ -0,0 +1,45 @@
+package com.github.PiotrDuma.payroll.domain.payment.method;
+
+import com.github.PiotrDuma.payroll.common.Address;
+import com.github.PiotrDuma.payroll.common.Salary;
+import com.github.PiotrDuma.payroll.domain.payment.method.api.PaymentDto;
+import com.github.PiotrDuma.payroll.domain.payment.method.api.PaymentMethod;
+import java.time.LocalDate;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+class MailPaymentMethod implements PaymentMethod {
+  Logger log = LoggerFactory.getLogger(HoldPaymentMethod.class);
+
+  private UUID id;
+  private Address address;
+  private List<PaymentEntity> payments;
+
+  public MailPaymentMethod(Address address) {
+    this.id = UUID.randomUUID();
+    this.address = address;
+    this.payments = new LinkedList<>();
+  }
+
+  @Override
+  public void executePayment(LocalDate date, Salary salary) {
+    PaymentEntity payment = new PaymentEntity(date, salary);
+    log.info("Proceed mail payment with address: " + address.toString());
+    this.payments.add(payment);
+    log.info("Payment "+ payment.getId().toString() + " executed");
+  }
+
+  public List<PaymentDto> getPayments() {
+    return payments.stream()
+        .map(PaymentEntity::toPaymentDto)
+        .collect(Collectors.toList());
+  }
+
+  public Address getAddress() {
+    return address;
+  }
+}
---
+++ src/test/java/com/github/PiotrDuma/payroll/domain/payment/method/MailPaymentMethodTest.java
@@ -0,0 +1,73 @@
+package com.github.PiotrDuma.payroll.domain.payment.method;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
+import com.github.PiotrDuma.payroll.common.Address;
+import com.github.PiotrDuma.payroll.common.Salary;
+import com.github.PiotrDuma.payroll.domain.payment.method.api.PaymentDto;
+import java.time.LocalDate;
+import java.util.UUID;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
+
+class MailPaymentMethodTest {
+  private static final Address ADDRESS = new Address("random address");
+
+  private ListAppender<ILoggingEvent> logWatcher;
+
+  @BeforeEach
+  void setup() {
+    logWatcher = new ListAppender<>();
+    logWatcher.start();
+    ((Logger) LoggerFactory.getLogger(HoldPaymentMethod.class)).addAppender(logWatcher);
+  }
+
+  @AfterEach
+  void teardown() {
+    ((Logger) LoggerFactory.getLogger(HoldPaymentMethod.class)).detachAndStopAllAppenders();
+  }
+
+  @Test
+  void shouldInitObjectWithValidValues(){
+    MailPaymentMethod paymentMethod = new MailPaymentMethod(ADDRESS);
+
+    assertEquals(ADDRESS, paymentMethod.getAddress());
+  }
+
+  @Test
+  void executePaymentShouldLog(){
+    String expectedMessage1 = "Proceed mail payment with address: random address";
+    MailPaymentMethod paymentMethod = new MailPaymentMethod(ADDRESS);
+    LocalDate date = LocalDate.of(2000, 1, 1);
+    Salary salary = new Salary(2000);
+    paymentMethod.executePayment(date, salary);
+    assertTrue(paymentMethod.getPayments().stream().findFirst().isPresent());
+    UUID id = paymentMethod.getPayments().stream().findFirst().get().id();
+
+    String expectedMessage2 = "Payment "+ id.toString() + " executed";
+
+    int size = logWatcher.list.size();
+    assertTrue(logWatcher.list.get(size - 2).getFormattedMessage().contains(expectedMessage1));
+    assertTrue(logWatcher.list.get(size - 1).getFormattedMessage().contains(expectedMessage2));
+  }
+
+  @Test
+  void executePaymentShouldAddNewPayment(){
+    MailPaymentMethod paymentMethod = new MailPaymentMethod(ADDRESS);
+    LocalDate date = LocalDate.of(2000, 1, 1);
+    Salary salary = new Salary(2000);
+    paymentMethod.executePayment(date, salary);
+
+    assertEquals(1, paymentMethod.getPayments().size());
+    assertTrue(paymentMethod.getPayments().stream().findFirst().isPresent());
+    PaymentDto payment = paymentMethod.getPayments().stream().findFirst().get();
+
+    assertEquals(date, payment.date());
+    assertEquals(salary.toString(), payment.salary().toString());
+  }
+}
