6a5173065a60736a7bf3059e0715e8a325aa1e52
PiotrDuma <piotrduma2014@gmail.com>
2024-02-09 19:27:05 +0100

    extend Employee add transaction by checking Id uniqueness

--- src/main/java/com/github/PiotrDuma/payroll/domain/employee/AbstractAddEmployeeTransaction.java
+++ src/main/java/com/github/PiotrDuma/payroll/domain/employee/AbstractAddEmployeeTransaction.java
@@ -8,8 +8,11 @@ import com.github.PiotrDuma.payroll.domain.payment.schedule.api.PaymentSchedule;
 import com.github.PiotrDuma.payroll.domain.employee.api.AddEmployeeTransaction;
 import com.github.PiotrDuma.payroll.domain.payment.classification.PaymentClassification;
 import com.github.PiotrDuma.payroll.domain.payment.method.api.PaymentMethod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 abstract class AbstractAddEmployeeTransaction implements AddEmployeeTransaction {
+  private static final Logger log = LoggerFactory.getLogger(AbstractAddEmployeeTransaction.class);
   protected final EmployeeRepository employeeRepository;
   protected final PaymentMethodFactory methodFactory;
   protected Address address;
@@ -33,11 +36,19 @@ abstract class AbstractAddEmployeeTransaction implements AddEmployeeTransaction 
     this.classification = getClassification();
     this.schedule = getPaymentSchedule();
     Employee employee = new Employee(name, address);
+    checkIfExists(employee.getId());
     employee.setPaymentClassification(this.classification);
     employee.setSchedule(this.schedule);
     employee.setPaymentMethod(methodFactory.getHoldPaymentMethod());
     this.employeeRepository.save(employee);
     return employee.getId();
+  }
+
+  private void checkIfExists(EmployeeId employeeId){
+    if(employeeRepository.findById(employeeId).isEmpty()){
+      log.error("AbstractAddEmployeeTransaction: Persistence error, UUID is not unique");
+      throw new RuntimeException("Unique ID persistence error during saving new Employee");
+    }
   }
 
   public Address getAddress() {
