1dfa0e4afb182d2b564d18585ead65d58b58f442
PiotrDuma <piotrduma2014@gmail.com>
2024-02-22 20:26:41 +0100

    extend test with ChangeHourlyClassificationTransaction  integration test

--- src/test/java/com/github/PiotrDuma/payroll/domain/payment/classification/hourly/HourlyClassificationIntegrationTest.java
+++ src/test/java/com/github/PiotrDuma/payroll/domain/payment/classification/hourly/HourlyClassificationIntegrationTest.java
@@ -1,12 +1,15 @@
 package com.github.PiotrDuma.payroll.domain.payment.classification.hourly;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.github.PiotrDuma.payroll.common.address.Address;
 import com.github.PiotrDuma.payroll.common.employeeId.EmployeeId;
+import com.github.PiotrDuma.payroll.common.salary.Salary;
 import com.github.PiotrDuma.payroll.domain.employee.api.AddEmployeeTransaction;
 import com.github.PiotrDuma.payroll.domain.employee.api.AddEmployeeTransactionFactory;
+import com.github.PiotrDuma.payroll.domain.employee.api.ChangeEmployeeService;
 import com.github.PiotrDuma.payroll.domain.employee.api.EmployeeName;
 import com.github.PiotrDuma.payroll.domain.employee.api.EmployeeResponse;
 import com.github.PiotrDuma.payroll.domain.employee.api.ReceiveEmployee;
@@ -29,6 +32,9 @@ class HourlyClassificationIntegrationTest {
   private AddEmployeeTransactionFactory addEmployeeFactory;
 
   @Autowired
+  private ChangeEmployeeService changeEmployeeService;
+
+  @Autowired
   private ReceiveEmployee repo;
 
   @Test
@@ -45,6 +51,31 @@ class HourlyClassificationIntegrationTest {
 
     assertTrue(paymentClassification instanceof HourlyClassificationEntity);
     HourlyClassificationEntity entity = (HourlyClassificationEntity) paymentClassification;
+
+    assertEquals(HOURLY_RATE, entity.getHourlyRate());
+  }
+
+  @Test
+  void changeEmployeeServiceShouldSetHourlyClassification(){
+    Salary salary = new Salary(1234);
+    AddEmployeeTransaction transaction = this.addEmployeeFactory.initSalariedEmployeeTransaction(
+        ADDRESS, EMPLOYEE_NAME, salary);
+
+    EmployeeId resultId = transaction.execute();
+
+    assertEquals(1, this.repo.findAll().size());
+
+    EmployeeResponse response = this.repo.find(resultId);
+    PaymentClassification paymentClassification = response.getPaymentClassification();
+    assertFalse(paymentClassification instanceof HourlyClassificationEntity);
+
+    this.changeEmployeeService.changeHourlyClassificationTransaction(resultId, HOURLY_RATE);
+
+    EmployeeResponse response2 = this.repo.find(resultId);
+    PaymentClassification paymentClassification2 = response2.getPaymentClassification();
+    assertTrue(paymentClassification2 instanceof HourlyClassificationEntity);
+
+    HourlyClassificationEntity entity = (HourlyClassificationEntity) paymentClassification2;
 
     assertEquals(HOURLY_RATE, entity.getHourlyRate());
   }
