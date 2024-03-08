72d0774fc48af7fe54b1e081c055bd0b054b8ee6
PiotrDuma <piotrduma2014@gmail.com>
2024-03-09 00:53:56 +0100

    refactor global exception handler

--- src/main/java/com/github/PiotrDuma/payroll/exception/DefaultExceptionHandler.java
+++ src/main/java/com/github/PiotrDuma/payroll/exception/DefaultExceptionHandler.java
@@ -1,56 +1,38 @@
 package com.github.PiotrDuma.payroll.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.NoHandlerFoundException;
-
 import java.time.Clock;
 import java.time.LocalDateTime;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-class DefaultExceptionHandler {
+class DefaultExceptionHandler extends ResponseEntityExceptionHandler {
     private final Clock clock;
 
     public DefaultExceptionHandler(Clock clock) {
         this.clock = clock;
     }
 
-    @ExceptionHandler({NoHandlerFoundException.class})
-    private ResponseEntity<ExceptionDto> handleNoHandlerFoundException(HttpServletRequest request) {
-        ExceptionDto exceptionDto = new ExceptionDto(
-                request.getRequestURI(),
-                "The requested URL does not exist",
-                HttpStatus.NOT_FOUND.value(),
-                LocalDateTime.now(clock)
-        );
-        return new ResponseEntity<>(exceptionDto, HttpStatus.NOT_FOUND);
-    }
-
     @ExceptionHandler({ResourceNotFoundException.class})
     private ResponseEntity<ExceptionDto> handleResourceNotFoundException(Exception ex, HttpServletRequest request) {
         ExceptionDto exceptionDto = new ExceptionDto(
-                request.getRequestURI(),
-                ex.getMessage(),
-                HttpStatus.NOT_FOUND.value(),
-                LocalDateTime.now(clock)
-        );
-        return new ResponseEntity<>(exceptionDto, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    private ResponseEntity<ExceptionDto> handleValidationException(Exception ex, HttpServletRequest request) {
-        ExceptionDto exceptionDto = new ExceptionDto(
             request.getRequestURI(),
             ex.getMessage(),
-            HttpStatus.BAD_REQUEST.value(),
+            HttpStatus.NOT_FOUND.value(),
             LocalDateTime.now(clock)
         );
-        return new ResponseEntity<>(exceptionDto, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(exceptionDto, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(InvalidArgumentException.class)
@@ -64,11 +46,30 @@ class DefaultExceptionHandler {
         return new ResponseEntity<>(exceptionDto, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler({HttpRequestMethodNotSupportedException.class})
-    private ResponseEntity<ExceptionDto> handleHttpRequestException(HttpServletRequest request) {
+    @Override //NoHandlerFoundException.class
+    @Nullable
+    protected ResponseEntity<Object> handleNoHandlerFoundException(
+        NoHandlerFoundException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        HttpServletRequest httpRequest = getHttpServletRequest(request);
         ExceptionDto exceptionDto = new ExceptionDto(
-            request.getRequestURI(),
-            "Request method '" + request.getMethod() + "' is not supported",
+            httpRequest.getRequestURI(),
+            "The requested URL does not exist",
+            HttpStatus.NOT_FOUND.value(),
+            LocalDateTime.now(clock)
+        );
+        return new ResponseEntity<>(exceptionDto, HttpStatus.NOT_FOUND);
+    }
+
+    @Override //HttpRequestMethodNotSupportedException.class
+    @Nullable
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(
+        HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        HttpServletRequest httpRequest = getHttpServletRequest(request);
+        ExceptionDto exceptionDto = new ExceptionDto(
+            httpRequest.getRequestURI(),
+            "Request method '" + httpRequest.getMethod() + "' is not supported",
             HttpStatus.BAD_REQUEST.value(),
             LocalDateTime.now(clock)
         );
@@ -84,6 +85,10 @@ class DefaultExceptionHandler {
                 LocalDateTime.now(clock)
         );
         return new ResponseEntity<>(exceptionDto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private HttpServletRequest getHttpServletRequest(WebRequest request) {
+        return ((ServletWebRequest) request).getRequest();
     }
 
     private record ExceptionDto(
