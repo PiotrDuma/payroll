c1a0bbe15e7833b96dcaa19419abd7025bc44db0
PiotrDuma <piotrduma2014@gmail.com>
2024-04-15 15:27:28 +0200

    update union controller

--- src/main/java/com/github/PiotrDuma/payroll/domain/union/UnionController.java
+++ src/main/java/com/github/PiotrDuma/payroll/domain/union/UnionController.java
@@ -1,11 +1,15 @@
 package com.github.PiotrDuma.payroll.domain.union;
 
+import com.github.PiotrDuma.payroll.common.amount.Amount;
+import com.github.PiotrDuma.payroll.common.employeeId.EmployeeId;
 import com.github.PiotrDuma.payroll.domain.union.api.UnionAffiliationService;
 import com.github.PiotrDuma.payroll.domain.union.api.UnionDto;
 import com.github.PiotrDuma.payroll.exception.ResourceNotFoundException;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import java.time.Clock;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -23,11 +27,14 @@ class UnionController {
   private static final String NOT_FOUND = "Union '%s' not found";
   private final UnionAffiliationRepository repo;
   private final UnionAffiliationService unionService;
+  private final Clock clock;
 
   @Autowired
-  public UnionController(UnionAffiliationRepository repo, UnionAffiliationService unionService) {
+  public UnionController(UnionAffiliationRepository repo, UnionAffiliationService unionService,
+      Clock clock) {
     this.repo = repo;
     this.unionService = unionService;
+    this.clock = clock;
   }
 
   @PostMapping("/unions")
@@ -50,14 +57,48 @@ class UnionController {
     return new ResponseEntity<>(union.toDto(), HttpStatus.OK);
   }
 
+  @PostMapping("/unions/{id}/{amount}")
+  public ResponseEntity<UnionDto> charge(@PathVariable("id") String id,
+      @PathVariable("amount") Double amount){
+    LocalDate date = LocalDate.ofInstant(clock.instant(), clock.getZone());
+    this.unionService.chargeMembers(UUID.fromString(id), new Amount(amount), date);
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+
+  @PostMapping("/unions/{id}/add")
+  public ResponseEntity<UnionDto> addMember(@PathVariable("id") String id,
+      @RequestBody @Valid EmployeeDto employee){
+    this.unionService.recordMembership(UUID.fromString(id),
+        new EmployeeId(UUID.fromString(employee.id())));
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+
+  @PostMapping("/unions/{id}/undo")
+  public ResponseEntity<UnionDto> revokeMembership(@PathVariable("id") String id,
+      @RequestBody @Valid EmployeeDto employee){
+    this.unionService.undoMembershipAffiliation(UUID.fromString(id),
+        new EmployeeId(UUID.fromString(employee.id())));
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+
   private record UnionNameDto(@NotNull @NotBlank String name) {
     private UnionNameDto(String name) {
         this.name = name;
       }
 
-      @Override
-      public String name() {
-        return name;
-      }
+    @Override
+    public String name() {
+      return name;
     }
+  }
+
+  private record EmployeeDto(@NotNull @NotBlank String id){
+    private EmployeeDto(String id){
+      this.id = id;
+    }
+    @Override
+    public String id(){
+      return id;
+    }
+  }
 }
