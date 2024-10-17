68f985bd0ddd5deeb987ddd4370156ea73cd6af5
PiotrDuma <piotrduma2014@gmail.com>
2024-10-17 23:13:59 +0200

    add payday controller

---
+++ src/main/java/com/github/PiotrDuma/payroll/services/payday/api/PaydayController.java
@@ -0,0 +1,28 @@
+package com.github.PiotrDuma.payroll.services.payday.api;
+
+import java.time.Clock;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PaydayController {
+  private static final Logger log = LoggerFactory.getLogger(PaydayController.class);
+  private static final String MESSAGE = "/payday request run at %s";
+  private final PaydayTransaction paydayTransaction;
+  private final Clock clock;
+
+  @Autowired
+  public PaydayController(PaydayTransaction paydayTransaction, Clock clock) {
+    this.paydayTransaction = paydayTransaction;
+    this.clock = clock;
+  }
+
+  @GetMapping("/payday")
+  public void run(){
+    log.info(String.format(MESSAGE, clock.instant().toString()));
+    this.paydayTransaction.execute();
+  }
+}
